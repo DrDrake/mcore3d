c5656636ffc85df7b71cbe3316e888a5bab2e1d2
mtlung <mtlung@080b3119-2d51-0410-af92-4d39592ae298>
2008-07-09 04:18:20 +0000

    *More documentation on Window and GlWindow *Added width() and height() getter for Window

--- MCD/Core/System/Window.h
+++ MCD/Core/System/Window.h
@@ -20,6 +20,24 @@ public:
 	virtual void onEvent(const Event& eventReceived) = 0;
 };	// IWindowListener
 
+/*!	An application window.
+	Example:
+	\code
+	Window window;
+	window.create(L"title='Hello world!';width=400;height=300");
+	window.setOptions(L"showCursor=0");
+
+	while(true) {
+		Event e;
+		window.popEvent(e, true);
+
+		if(e.Type == Event::Closed) {
+			window.destroy();
+			break;
+		}
+	}
+	\endcode
+ */
 class MCD_CORE_API Window : protected IWindowListener, Noncopyable
 {
 public:
@@ -37,25 +55,47 @@ public:
 	//! Get the platform dependent handle to this window
 	Handle handle();
 
-	/*!	Create a new window
+	/*!	Create a new window.
 	 */
 	void create(sal_in_z_opt const wchar_t* options=nullptr) throw(std::exception);
 
-	/*!	Create a window from an existing control
+	/*!	Create a window from an existing control.
 	 */
 	void create(Handle existingControl, sal_in_z_opt const wchar_t* options=nullptr) throw(std::exception);
 
 	void setOptions(sal_in_z sal_notnull const wchar_t* options);
 
 	/*!	Destroy the window and perform necessary cleanup.
+		\note Will be invoked in ~Window()
 	 */
 	void destroy();
 
+	/*!	Extract events from the OS API.
+		This function extract and translates the platform dependent messages into our Event class,
+		then invoke onEvent() to inform the user.
+	 */
 	void processEvent(bool blocking);
 
+	/*!	Pop an event out of the event queue.
+		Implicitly call processEvent() if the event queue is empty.
+		\param blocing If it's true, the function will block until an event happen.
+		\return false if the event queue is empty, meaningfull for non-blocking mode only.
+	 */
 	bool popEvent(Event& event, bool blocking);
 
+	//! Width of the window in pixel.
+	uint width() const;
+
+	//! Height of the window in pixel.
+	uint height() const;
+
 protected:
+	/*!	Invoked by processEvent() to tell the user about the event happened.
+		The implementation of Window::onEvent() will put the event into it's
+		event queue so that user can query about it using popEvent().
+		User can override this function and perform different actions base on
+		the event rather than put it into the event queue.
+	 */
 	sal_override void onEvent(const Event& eventReceived);
 
 protected:
