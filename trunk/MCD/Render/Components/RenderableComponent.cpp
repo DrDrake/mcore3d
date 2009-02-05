cb679f87c9ded5874b34000382d55f4dd8eaa621
mtlung <mtlung@080b3119-2d51-0410-af92-4d39592ae298>
2009-02-05 08:04:08 +0000

    *RenderableComponent will also take Entity->worldTransform into account

--- trunk/MCD/Render/Components/RenderableComponent.cpp
+++ trunk/MCD/Render/Components/RenderableComponent.cpp
@@ -2,17 +2,26 @@
 #include "RenderableComponent.h"
 #include "../../Core/Entity/Entity.h"
 #include "../../Core/System/Utility.h"
+#include "../../../3Party/glew/glew.h"
 
 namespace MCD {
 
 void RenderableComponent::traverseEntities(Entity* entityNode)
 {
+	glMatrixMode(GL_MODELVIEW);
+
 	for(EntityPreorderIterator itr(entityNode); !itr.ended(); itr.next())
 	{
 		RenderableComponent* renderable = polymorphic_downcast<RenderableComponent*>(itr->findComponent(typeid(RenderableComponent)));
 		if(!renderable)
 			continue;
+
+		glPushMatrix();
+		glMultTransposeMatrixf(itr->worldTransform().getPtr());
+
 		renderable->render();
+
+		glPopMatrix();
 	}
 }
 
