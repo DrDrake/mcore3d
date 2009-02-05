80e205ac2a540c69ab401dcba2968953692924ff
mtlung <mtlung@080b3119-2d51-0410-af92-4d39592ae298>
2009-02-05 08:02:58 +0000

    *One more entity for the MeshComponentTest

--- trunk/Test/RenderTest/MeshComponentTest.cpp
+++ trunk/Test/RenderTest/MeshComponentTest.cpp
@@ -18,14 +18,33 @@ TEST(MeshComponentTest)
 			BasicGlWindow(L"title=MeshComponentTest;width=800;height=600;fullscreen=0;FSAA=4"),
 			mResourceManager(*createDefaultFileSystem())
 		{
-			{	// Setup the entity
+			{	// Setup entity 1
 				Entity* e = new Entity;
 				e->name = L"ChamferBox 1";
 				e->link(&mRootNode);
+				e->localTransform = Mat44f::rotate(0, Mathf::cPiOver4(), 0);
 
 				// Setup the chamfer box mesh
 				MeshPtr mesh = new Mesh(L"");
 				ChamferBoxBuilder chamferBoxBuilder(0.4f, 10);
+				chamferBoxBuilder.commit(*mesh, MeshBuilder::Static);
+
+				// Add component
+				MeshComponent* c = new MeshComponent;
+				c->mesh = mesh;
+				c->effect = static_cast<Effect*>(mResourceManager.load(L"Material/test.fx.xml").get());
+				e->addComponent(c);
+			}
+
+			{	// Setup entity 2
+				Entity* e = new Entity;
+				e->name = L"Sphere 1";
+				e->link(&mRootNode);
+				e->localTransform.setTranslation(Vec3f(1, 0, 0));
+
+				// Setup the chamfer box mesh
+				MeshPtr mesh = new Mesh(L"");
+				ChamferBoxBuilder chamferBoxBuilder(1.0f, 10);
 				chamferBoxBuilder.commit(*mesh, MeshBuilder::Static);
 
 				// Add component
