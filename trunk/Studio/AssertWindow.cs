9884139041ac3808262a2cca606aceff2044ffc7
mtlung <mtlung@080b3119-2d51-0410-af92-4d39592ae298>
2009-02-17 07:55:18 +0000

    *Make the assert window tree view more lazy, only query the file system when a node is being expanded

--- trunk/Studio/AssertWindow.cs
+++ trunk/Studio/AssertWindow.cs
@@ -13,7 +13,7 @@ namespace Studio
 		public void refresh(string dir)
 		{
 			treeView.Nodes.Clear();
-			populateTree(dir, treeView.Nodes);
+			populateTree(dir, treeView.Nodes, 2);
 		}
 
 		/// <summary>
@@ -21,30 +21,63 @@ namespace Studio
 		/// </summary>
 		/// <param name="dir">The path of the directory</param>
 		/// <param name="nodes">The "master" node's collection, to populate</param>
+		/// <param name="depth">The recursive level</param>
 		///	<seealso cref="http://www.dreamincode.net/code/snippet2591.htm"/>
-		void populateTree(string dir, TreeNodeCollection nodes)
+		void populateTree(string dir, TreeNodeCollection nodes, int depth)
 		{
+			if (depth <= 0)
+				return;
+
+			nodes.Clear();
+
 			// Get the information of the directory
 			DirectoryInfo directory = new DirectoryInfo(dir);
 
-			// Loop through each subdirectory
-			foreach (DirectoryInfo d in directory.GetDirectories())
+			// Put the directory.GetDirectories() in a try block in case any error.
+			try
 			{
-				// Creates a new node
-				TreeNode t = new TreeNode(d.Name);
-				// Populate the new node recursively
-				populateTree(d.FullName, t.Nodes);
-				nodes.Add(t);	// Add the node to the "master" node
+				// Loop through each subdirectory
+				foreach (DirectoryInfo d in directory.GetDirectories())
+				{
+					// Creates a new node
+					TreeNode t = new TreeNode(d.Name);
+					// Populate the new node recursively
+					populateTree(d.FullName, t.Nodes, depth - 1);
+					nodes.Add(t);	// Add the node to the "master" node
+				}
+			}
+			catch
+			{
 			}
 
-			// Lastly, loop through each file in the directory, and add these as nodes
-			foreach (FileInfo f in directory.GetFiles())
+			try
 			{
-				// Create a new node
-				TreeNode t = new TreeNode(f.Name);
-				// Add it to the "master"
-				nodes.Add(t);
+				// Lastly, loop through each file in the directory, and add these as nodes
+				foreach (FileInfo f in directory.GetFiles())
+				{
+					// Create a new node
+					TreeNode t = new TreeNode(f.Name);
+					t.ImageIndex = 1;
+					t.SelectedImageIndex = 1;
+					// Add it to the "master"
+					nodes.Add(t);
+				}
 			}
+			catch
+			{
+			}
+		}
+
+		/// <summary>
+		/// Only add more nodes when necessary.
+		/// </summary>
+		private void treeView_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+		{
+			treeView.BeginUpdate();
+
+			populateTree("Media/" + e.Node.FullPath, e.Node.Nodes, 2);
+
+			treeView.EndUpdate();
 		}
 	}
 }
