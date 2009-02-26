4ee3a4f706be433a64050752bd2436113fa57049
mtlung <mtlung@080b3119-2d51-0410-af92-4d39592ae298>
2009-02-26 12:19:27 +0000

    *Remove unused using declarations

--- trunk/Studio/MainForm.cs
+++ trunk/Studio/MainForm.cs
@@ -1,12 +1,7 @@
-﻿using Binding;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
+using Binding;
 using WeifenLuo.WinFormsUI.Docking;
 
 namespace Studio
