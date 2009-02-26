4da9461ff3b64eaf142a9fc662e892ff909c3cb9
mtlung <mtlung@080b3119-2d51-0410-af92-4d39592ae298>
2009-02-26 12:19:27 +0000

    *Remove unused using declarations

--- Studio/DockContent.cs
+++ Studio/DockContent.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-using WeifenLuo.WinFormsUI.Docking;
-
-namespace Studio
+﻿namespace Studio
 {
 	public partial class DockContent : WeifenLuo.WinFormsUI.Docking.DockContent
 	{
