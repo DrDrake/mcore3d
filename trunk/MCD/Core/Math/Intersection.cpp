851dae55aceb5d4b3e4bb71a47f80c96da1549a4
mtlung <mtlung@080b3119-2d51-0410-af92-4d39592ae298>
2009-04-01 15:16:29 +0000

    *More constructor for Plane *Ray - Plane intersection fix

--- trunk/MCD/Core/Math/Intersection.cpp
+++ trunk/MCD/Core/Math/Intersection.cpp
@@ -6,14 +6,15 @@
 
 namespace MCD {
 
+// Reference: http://www.cs.toronto.edu/~smalik/418/tutorial8_ray_primitive_intersections.pdf
 bool Intersects(const Ray& ray, const Plane& plane, float& distanceAlongRay)
 {
 	float denom = plane.normal.dot(ray.unitDirection);
 
 	if(!Mathf::isNearZero(denom))
 	{
-		float nom = plane.normal.dot(ray.origin) + plane.constant;
-		distanceAlongRay = nom / denom;
+		float nom = plane.normal.dot(ray.origin) + plane.d;
+		distanceAlongRay = -nom / denom;
 		return true;
 	}
 
