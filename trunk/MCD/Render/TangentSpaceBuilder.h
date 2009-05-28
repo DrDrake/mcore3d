ae5e99f95a2d97f3294e3531f16b2855bc0b2c4d
ming4883 <ming4883@080b3119-2d51-0410-af92-4d39592ae298>
2009-05-28 15:05:15 +0000

    added TangentSpaceBuilder to Render

---
+++ trunk/MCD/Render/TangentSpaceBuilder.h
@@ -0,0 +1,41 @@
+#ifndef __MCD_RENDER_TANGENTSPACEBUILDER__
+#define __MCD_RENDER_TANGENTSPACEBUILDER__
+
+#include "ShareLib.h"
+#include "../Core/Math/Vec2.h"
+#include "../Core/Math/Vec3.h"
+
+namespace MCD
+{
+
+class MeshBuilder;
+
+/*! The compute tangent space for MeshBuilders.
+
+	Usage:
+
+	TangentSpaceBuilder tsbuilder;
+
+	Mesh::DataType sourceUV = Mesh::TextureCoord0;
+	Mesh::DataType outputUV = Mesh::TextureCoord1;
+
+	// please make sure meshBuilder have allocate the space for
+	// outputUV (as an array of Vec3)
+	tsbuilder.compute( meshBuilder, sourceUV, outputUV );
+*/
+class MCD_RENDER_API TangentSpaceBuilder
+{
+public:
+	void compute( MeshBuilder& builder, int uvDataType, int tangentDataType );
+
+protected:
+	static void computeTangentBasis(
+		const Vec3f& P1, const Vec3f& P2, const Vec3f& P3, 
+		const Vec2f& UV1, const Vec2f& UV2, const Vec2f& UV3,
+		Vec3f &tangent, Vec3f &bitangent
+		);
+};
+
+}	// namespace MCD
+
+#endif	// __MCD_RENDER_TANGENTSPACEBUILDER__
