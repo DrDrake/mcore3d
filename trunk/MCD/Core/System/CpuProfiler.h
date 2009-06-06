258e6c129eff50cc20a726dc38b5de0425fb64ac
mtlung <mtlung@080b3119-2d51-0410-af92-4d39592ae298>
2009-06-06 08:58:14 +0000

    *Added cpu profiler

---
+++ trunk/MCD/Core/System/CpuProfiler.h
@@ -0,0 +1,76 @@
+#ifndef __MCD_CORE_SYSTEM_CPUPROFILER__
+#define __MCD_CORE_SYSTEM_CPUPROFILER__
+
+#include "CallstackProfiler.h"
+#include "Timer.h"
+
+namespace MCD {
+
+/*!	To use together with CpuProfiler
+ */
+class MCD_CORE_API CpuProfilerNode : public CallstackNode
+{
+public:
+	CpuProfilerNode(sal_in_z const char name[], sal_maybenull CallstackNode* parent=nullptr);
+
+// Operations
+	sal_override void begin();
+
+	sal_override void end();
+
+	sal_override CallstackNode* createNode(sal_in_z const char name[], sal_maybenull CallstackNode* parent);
+
+	/*!	Reset the collected statistic including all child nodes.
+		For saving memory, those call stack node with zero callCount will be deleted.
+	 */
+	void reset();
+
+// Attributes
+	//! Get the time spend of only this function (excluding any child call)
+	float selfTime() const;
+
+	size_t callCount;
+
+	//! The time spend between the call of begin() and end()
+	TimeInterval inclusiveTime;
+
+	Timer timer;
+};	// CpuProfilerNode
+
+/*!	
+ */
+class MCD_CORE_API CpuProfiler : public CallstackProfiler
+{
+public:
+	static CpuProfiler& singleton();
+
+// Operations
+	void setRootNode(sal_maybenull CallstackNode* root);
+
+	/*!	Inform the profiler a new iteration begins.
+		This function is most likely to be called after each iteration of the main loop.
+		\note Do not call this function between begin() and end()
+	 */
+	void nextFrame();
+
+	/*!	Reset the state of the profiler.
+		The profiler keeps calculating statistic for calculating average values later on.
+		Therefore, if you want to take average over 10 frames, call this function every 10 frames.
+		\note Do not call this function between begin() and end()
+	 */
+	void reset();
+
+	std::string defaultReport(size_t nameLength=100) const;
+
+// Attributes
+	//! Frame per second.
+	float fps() const;
+
+	size_t frameCount;	//! Number of frame elasped since last reset
+	TimeInterval timeSinceLastReset;
+	Timer timer;
+};	// CpuProfiler
+
+}	// namespace MCD
+
+#endif	// __MCD_CORE_SYSTEM_CPUPROFILER__
