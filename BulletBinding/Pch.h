0a88233d1ea78946148c0992855c58e1d7b73dfa
mtlung <mtlung@080b3119-2d51-0410-af92-4d39592ae298>
2009-06-14 15:39:21 +0000

    *Bullet warning fixed

--- BulletBinding/Pch.h
+++ BulletBinding/Pch.h
@@ -1,3 +1,13 @@
+#include "../MCD/Core/Math/Vec3.h"
+
+#ifdef _MSC_VER
+#	pragma warning (push)
+#	pragma warning (disable : 4100)
+#endif
+
 #include "../3Party/bullet/btBulletDynamicsCommon.h"
 #include "../3Party/bullet/btBulletCollisionCommon.h"
-#include "../MCD/Core/Math/Vec3.h"
+
+#ifdef _MSC_VER
+#	pragma warning (pop)
+#endif
