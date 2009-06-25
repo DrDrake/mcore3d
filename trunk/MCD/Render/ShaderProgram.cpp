553767ed39f229734b014009fd62f0e1cb9cfed5
ming4883 <ming4883@080b3119-2d51-0410-af92-4d39592ae298>
2009-06-25 13:55:26 +0000

    added uniform functions to ShaderProgram

--- trunk/MCD/Render/ShaderProgram.cpp
+++ trunk/MCD/Render/ShaderProgram.cpp
@@ -86,4 +86,80 @@ void ShaderProgram::getLog(std::string& log)
 	glGetProgramInfoLog(handle, len, nullptr, &log[0]);
 }
 
+void ShaderProgram::uniform1f(const char* name, float v0)
+{
+}
+
+void ShaderProgram::uniform2f(const char* name, float v0, float v1)
+{
+}
+
+void ShaderProgram::uniform3f(const char* name, float v0, float v1, float v2)
+{
+}
+
+void ShaderProgram::uniform4f(const char* name, float v0, float v1, float v2, float v3)
+{
+}
+
+void ShaderProgram::uniform1fv(const char* name, size_t count, const float* value)
+{
+}
+
+void ShaderProgram::uniform2fv(const char* name, size_t count, const float* value)
+{
+}
+
+void ShaderProgram::uniform3fv(const char* name, size_t count, const float* value)
+{
+}
+
+void ShaderProgram::uniform4fv(const char* name, size_t count, const float* value)
+{
+}
+
+void ShaderProgram::uniform1i(const char* name, int v0)
+{
+}
+
+void ShaderProgram::uniform2i(const char* name, int v0, int v1)
+{
+}
+
+void ShaderProgram::uniform3i(const char* name, int v0, int v1, int v2)
+{
+}
+
+void ShaderProgram::uniform4i(const char* name, int v0, int v1, int v2, int v3)
+{
+}
+
+void ShaderProgram::uniform1iv(const char* name, size_t count, const int* value)
+{
+}
+
+void ShaderProgram::uniform2iv(const char* name, size_t count, const int* value)
+{
+}
+
+void ShaderProgram::uniform3iv(const char* name, size_t count, const int* value)
+{
+}
+
+void ShaderProgram::uniform4iv(const char* name, size_t count, const int* value)
+{
+}
+
+void ShaderProgram::uniformMatrix2fv(const char* name, size_t count, const float* value)
+{
+}
+
+void ShaderProgram::uniformMatrix3fv(const char* name, size_t count, const float* value)
+{
+}
+
+void ShaderProgram::uniformMatrix4fv(const char* name, size_t count, const float* value)
+{
+}
+
 }	// namespace MCD
