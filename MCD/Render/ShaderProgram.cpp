94c1efdfaab55fa7f9930a2d5f97e18fa60012a3
ming4883 <ming4883@080b3119-2d51-0410-af92-4d39592ae298>
2009-06-25 17:09:22 +0000

    added uniforms supports to ShaderProgram

--- MCD/Render/ShaderProgram.cpp
+++ MCD/Render/ShaderProgram.cpp
@@ -2,17 +2,55 @@
 #include "ShaderProgram.h"
 #include "Shader.h"
 #include "../../3Party/glew/glew.h"
+#include <map>
+#include <vector>
 #include <algorithm>
 
 namespace MCD {
 
+class ShaderProgram::Impl
+{
+public:
+	struct CStrKey
+	{
+		const char* key;
+		CStrKey(const char* key_) : key(key_) {}
+		
+		bool operator < (const CStrKey& rhs) const { return strcmp(key, rhs.key) < 0; }
+		bool operator == (const CStrKey& rhs) const { return strcmp(key, rhs.key) == 0; }
+	};
+
+	inline int getUniform(int program, const char* name)
+	{
+		CStrKey key(name);
+		Uniforms::iterator i = mUniforms.find(key);
+
+		if(i != mUniforms.end())
+			return i->second;
+
+		int loc = glGetUniformLocation(program, name);
+		mUniforms.insert(std::make_pair(key, loc));
+		
+		return loc;
+	}
+
+	typedef std::vector<ShaderPtr> Shaders;
+	typedef std::map<CStrKey, int> Uniforms;
+
+	Uniforms mUniforms;
+	Shaders mShaders;
+};
+
+
 ShaderProgram::ShaderProgram()
 	: handle(0)
+	, mImpl(new Impl)
 {}
 
 ShaderProgram::~ShaderProgram()
 {
 	destroy();
+	delete mImpl;
 }
 
 void ShaderProgram::create()
@@ -36,22 +74,22 @@ void ShaderProgram::attach(Shader& shader)
 {
 	detach(shader);
 	glAttachShader(handle, shader.handle());
-	mShaders.push_back(&shader);
+	mImpl->mShaders.push_back(&shader);
 }
 
 void ShaderProgram::detach(Shader& shader)
 {
-	Shaders::const_iterator i = std::find(mShaders.begin(), mShaders.end(), &shader);
-	if(i != mShaders.end())
+	Impl::Shaders::const_iterator i = std::find(mImpl->mShaders.begin(), mImpl->mShaders.end(), &shader);
+	if(i != mImpl->mShaders.end())
 		glDetachShader(handle, shader.handle());
 }
 
 void ShaderProgram::detachAll()
 {
-	for(Shaders::const_iterator i=mShaders.begin(); i!=mShaders.end(); ++i)
+	for(Impl::Shaders::const_iterator i=mImpl->mShaders.begin(); i!=mImpl->mShaders.end(); ++i)
 		glDetachShader(handle, (*i)->handle());
 
-	mShaders.clear();
+	mImpl->mShaders.clear();
 }
 
 bool ShaderProgram::link()
@@ -88,78 +126,97 @@ void ShaderProgram::getLog(std::string& log)
 
 void ShaderProgram::uniform1f(const char* name, float v0)
 {
+	glUniform1f(mImpl->getUniform(handle, name), v0);
 }
 
 void ShaderProgram::uniform2f(const char* name, float v0, float v1)
 {
+	glUniform2f(mImpl->getUniform(handle, name), v0, v1);
 }
 
 void ShaderProgram::uniform3f(const char* name, float v0, float v1, float v2)
 {
+	glUniform3f(mImpl->getUniform(handle, name), v0, v1, v2);
 }
 
 void ShaderProgram::uniform4f(const char* name, float v0, float v1, float v2, float v3)
 {
+	glUniform4f(mImpl->getUniform(handle, name), v0, v1, v2, v3);
 }
 
 void ShaderProgram::uniform1fv(const char* name, size_t count, const float* value)
 {
+	glUniform1fv(mImpl->getUniform(handle, name), count, value);
 }
 
 void ShaderProgram::uniform2fv(const char* name, size_t count, const float* value)
 {
+	glUniform2fv(mImpl->getUniform(handle, name), count, value);
 }
 
 void ShaderProgram::uniform3fv(const char* name, size_t count, const float* value)
 {
+	glUniform3fv(mImpl->getUniform(handle, name), count, value);
 }
 
 void ShaderProgram::uniform4fv(const char* name, size_t count, const float* value)
 {
+	glUniform4fv(mImpl->getUniform(handle, name), count, value);
 }
 
 void ShaderProgram::uniform1i(const char* name, int v0)
 {
+	glUniform1i(mImpl->getUniform(handle, name), v0);
 }
 
 void ShaderProgram::uniform2i(const char* name, int v0, int v1)
 {
+	glUniform2i(mImpl->getUniform(handle, name), v0, v1);
 }
 
 void ShaderProgram::uniform3i(const char* name, int v0, int v1, int v2)
 {
+	glUniform3i(mImpl->getUniform(handle, name), v0, v1, v2);
 }
 
 void ShaderProgram::uniform4i(const char* name, int v0, int v1, int v2, int v3)
 {
+	glUniform4i(mImpl->getUniform(handle, name), v0, v1, v2, v3);
 }
 
 void ShaderProgram::uniform1iv(const char* name, size_t count, const int* value)
 {
+	glUniform1iv(mImpl->getUniform(handle, name), count, value);
 }
 
 void ShaderProgram::uniform2iv(const char* name, size_t count, const int* value)
 {
+	glUniform2iv(mImpl->getUniform(handle, name), count, value);
 }
 
 void ShaderProgram::uniform3iv(const char* name, size_t count, const int* value)
 {
+	glUniform3iv(mImpl->getUniform(handle, name), count, value);
 }
 
 void ShaderProgram::uniform4iv(const char* name, size_t count, const int* value)
 {
+	glUniform4iv(mImpl->getUniform(handle, name), count, value);
 }
 
-void ShaderProgram::uniformMatrix2fv(const char* name, size_t count, const float* value)
+void ShaderProgram::uniformMatrix2fv(const char* name, size_t count, bool transpose, const float* value)
 {
+	glUniformMatrix2fv(mImpl->getUniform(handle, name), count, transpose, value);
 }
 
-void ShaderProgram::uniformMatrix3fv(const char* name, size_t count, const float* value)
+void ShaderProgram::uniformMatrix3fv(const char* name, size_t count, bool transpose, const float* value)
 {
+	glUniformMatrix3fv(mImpl->getUniform(handle, name), count, transpose, value);
 }
 
-void ShaderProgram::uniformMatrix4fv(const char* name, size_t count, const float* value)
+void ShaderProgram::uniformMatrix4fv(const char* name, size_t count, bool transpose, const float* value)
 {
+	glUniformMatrix4fv(mImpl->getUniform(handle, name), count, transpose, value);
 }
 
 }	// namespace MCD
