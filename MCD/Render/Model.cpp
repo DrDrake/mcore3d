6385759945246dea34ac93c3168e4c9443c19f85
ming4883 <ming4883@080b3119-2d51-0410-af92-4d39592ae298>
2009-07-04 02:39:54 +0000

    changed Model to store Material2* instead of IMaterial* changed Material2::clone() to return Material2* instead of IMaterial* converted some space character to tabs in several source files

--- MCD/Render/Model.cpp
+++ MCD/Render/Model.cpp
@@ -13,14 +13,14 @@ Model::MeshAndMaterial::~MeshAndMaterial()
 
 void Model::draw()
 {
-    for(MeshAndMaterial* meshAndMat = mMeshes.begin(); meshAndMat != mMeshes.end(); meshAndMat = meshAndMat->next())
-    {
-        for(size_t passId = 0; passId < meshAndMat->material->getPassCount(); ++passId)
-        {
-            meshAndMat->material->preRender(passId);
-            meshAndMat->mesh->draw();
-            meshAndMat->material->postRender(passId);
-        }
+	for(MeshAndMaterial* meshAndMat = mMeshes.begin(); meshAndMat != mMeshes.end(); meshAndMat = meshAndMat->next())
+	{
+		for(size_t passId = 0; passId < meshAndMat->material->getPassCount(); ++passId)
+		{
+			meshAndMat->material->preRender(passId);
+			meshAndMat->mesh->draw();
+			meshAndMat->material->postRender(passId);
+		}
 	}
 }
 
