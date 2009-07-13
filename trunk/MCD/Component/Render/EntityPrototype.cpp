15a5c244e6e2a2977944fa0919140a63d676a88e
ming4883 <ming4883@080b3119-2d51-0410-af92-4d39592ae298>
2009-07-13 16:57:26 +0000

    added EntityPrototype.cpp & EntityPrototype.h

---
+++ trunk/MCD/Component/Render/EntityPrototype.cpp
@@ -0,0 +1,15 @@
+#include "Pch.h"
+#include "EntityPrototype.h"
+
+namespace MCD {
+
+EntityPrototype::EntityPrototype(const Path& fileId)
+	: Resource(fileId)
+{
+}
+
+EntityPrototype::~EntityPrototype()
+{
+}
+
+}	// namespace MCD
