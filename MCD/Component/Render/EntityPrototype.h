0d590ab1838642bb98d6d7c750b3b30c24564c1a
ming4883 <ming4883@080b3119-2d51-0410-af92-4d39592ae298>
2009-07-13 16:57:26 +0000

    added EntityPrototype.cpp & EntityPrototype.h

---
+++ MCD/Component/Render/EntityPrototype.h
@@ -0,0 +1,27 @@
+#ifndef __MCD_COMPONENT_RENDER_ENTITYPROTOTYPE__
+#define __MCD_COMPONENT_RENDER_ENTITYPROTOTYPE__
+
+#include "../ShareLib.h"
+#include "../../Core/System/Resource.h"
+#include "../../Core/System/Noncopyable.h"
+#include "../../Core/Entity/Entity.h"
+
+namespace MCD {
+
+class IResourceManager;
+
+class MCD_COMPONENT_API EntityPrototype : public Resource, Noncopyable
+{
+public:
+	explicit EntityPrototype(const Path& fileId);
+
+	sal_override ~EntityPrototype();
+
+	/*! When an EntityPrototype is loaded, mEntity is not be NULL */
+	std::auto_ptr<Entity> mEntity;
+
+};	// EntityPrototype
+
+}	// namespace MCD
+
+#endif	// __MCD_COMPONENT_RENDER_ENTITYPROTOTYPE__
