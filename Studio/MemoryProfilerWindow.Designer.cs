c519249b24357b26000dc218dfd1949970a10471
mtlung <mtlung@080b3119-2d51-0410-af92-4d39592ae298>
2009-09-25 04:14:08 +0000

    *Improved the UI of MemoryProfilerWindow, added a pause button

--- Studio/MemoryProfilerWindow.Designer.cs
+++ Studio/MemoryProfilerWindow.Designer.cs
@@ -39,16 +39,18 @@
 			Aga.Controls.Tree.NodeControls.NodeTextBox nodeTextBox1;
 			Aga.Controls.Tree.NodeControls.NodeDecimalTextBox nodeDecimalTextBox1;
 			Aga.Controls.Tree.NodeControls.NodeDecimalTextBox nodeDecimalTextBox2;
-			Aga.Controls.Tree.NodeControls.NodeDecimalTextBox nodeDecimalTextBox3;
-			Aga.Controls.Tree.NodeControls.NodeDecimalTextBox nodeDecimalTextBox4;
 			Aga.Controls.Tree.NodeControls.NodeDecimalTextBox nodeDecimalTextBox5;
 			Aga.Controls.Tree.NodeControls.NodeDecimalTextBox nodeDecimalTextBox6;
+			Aga.Controls.Tree.NodeControls.NodeTextBox nodeTextBox2;
+			Aga.Controls.Tree.NodeControls.NodeTextBox nodeTextBox3;
+			System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+			System.Windows.Forms.Panel panel1;
 			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
 			this.timer1 = new System.Windows.Forms.Timer(this.components);
 			this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.treeViewAdv1 = new Aga.Controls.Tree.TreeViewAdv();
-			this.panel1 = new System.Windows.Forms.Panel();
 			treeColumn1 = new Aga.Controls.Tree.TreeColumn();
 			treeColumn2 = new Aga.Controls.Tree.TreeColumn();
 			treeColumn3 = new Aga.Controls.Tree.TreeColumn();
@@ -59,11 +61,14 @@
 			nodeTextBox1 = new Aga.Controls.Tree.NodeControls.NodeTextBox();
 			nodeDecimalTextBox1 = new Aga.Controls.Tree.NodeControls.NodeDecimalTextBox();
 			nodeDecimalTextBox2 = new Aga.Controls.Tree.NodeControls.NodeDecimalTextBox();
-			nodeDecimalTextBox3 = new Aga.Controls.Tree.NodeControls.NodeDecimalTextBox();
-			nodeDecimalTextBox4 = new Aga.Controls.Tree.NodeControls.NodeDecimalTextBox();
 			nodeDecimalTextBox5 = new Aga.Controls.Tree.NodeControls.NodeDecimalTextBox();
 			nodeDecimalTextBox6 = new Aga.Controls.Tree.NodeControls.NodeDecimalTextBox();
-			this.panel1.SuspendLayout();
+			nodeTextBox2 = new Aga.Controls.Tree.NodeControls.NodeTextBox();
+			nodeTextBox3 = new Aga.Controls.Tree.NodeControls.NodeTextBox();
+			flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			panel1 = new System.Windows.Forms.Panel();
+			flowLayoutPanel1.SuspendLayout();
+			panel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// treeColumn1
@@ -94,7 +99,7 @@
 			treeColumn4.Header = "TkBytes";
 			treeColumn4.SortOrder = System.Windows.Forms.SortOrder.None;
 			treeColumn4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-			treeColumn4.TooltipText = "Total allocated memory in kilo bytes (including child call)";
+			treeColumn4.TooltipText = "Total allocated memory rounded to nearest kilo bytes (including child call)";
 			treeColumn4.Width = 60;
 			// 
 			// treeColumn5
@@ -102,7 +107,7 @@
 			treeColumn5.Header = "SkBytes";
 			treeColumn5.SortOrder = System.Windows.Forms.SortOrder.None;
 			treeColumn5.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-			treeColumn5.TooltipText = "Self allocated memory in kilo bytes (child call not included)";
+			treeColumn5.TooltipText = "Self allocated memory orunded to nearest kilo bytes (child call not included)";
 			treeColumn5.Width = 60;
 			// 
 			// treeColumn6
@@ -144,22 +149,6 @@
 			nodeDecimalTextBox2.ParentColumn = treeColumn3;
 			nodeDecimalTextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
 			// 
-			// nodeDecimalTextBox3
-			// 
-			nodeDecimalTextBox3.DataPropertyName = "TkBytes";
-			nodeDecimalTextBox3.IncrementalSearchEnabled = true;
-			nodeDecimalTextBox3.LeftMargin = 3;
-			nodeDecimalTextBox3.ParentColumn = treeColumn4;
-			nodeDecimalTextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-			// 
-			// nodeDecimalTextBox4
-			// 
-			nodeDecimalTextBox4.DataPropertyName = "SkBytes";
-			nodeDecimalTextBox4.IncrementalSearchEnabled = true;
-			nodeDecimalTextBox4.LeftMargin = 3;
-			nodeDecimalTextBox4.ParentColumn = treeColumn5;
-			nodeDecimalTextBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-			// 
 			// nodeDecimalTextBox5
 			// 
 			nodeDecimalTextBox5.DataPropertyName = "SCountPerFrame";
@@ -176,18 +165,60 @@
 			nodeDecimalTextBox6.ParentColumn = treeColumn7;
 			nodeDecimalTextBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
 			// 
+			// nodeTextBox2
+			// 
+			nodeTextBox2.DataPropertyName = "TkBytesAsString";
+			nodeTextBox2.IncrementalSearchEnabled = true;
+			nodeTextBox2.LeftMargin = 3;
+			nodeTextBox2.ParentColumn = treeColumn4;
+			nodeTextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			// 
+			// nodeTextBox3
+			// 
+			nodeTextBox3.DataPropertyName = "SkBytesAsString";
+			nodeTextBox3.IncrementalSearchEnabled = true;
+			nodeTextBox3.LeftMargin = 3;
+			nodeTextBox3.ParentColumn = treeColumn5;
+			nodeTextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			// 
+			// flowLayoutPanel1
+			// 
+			flowLayoutPanel1.AutoSize = true;
+			flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			flowLayoutPanel1.Controls.Add(this.button1);
+			flowLayoutPanel1.Controls.Add(this.button2);
+			flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Right;
+			flowLayoutPanel1.Location = new System.Drawing.Point(568, 0);
+			flowLayoutPanel1.Name = "flowLayoutPanel1";
+			flowLayoutPanel1.Size = new System.Drawing.Size(101, 23);
+			flowLayoutPanel1.TabIndex = 2;
+			flowLayoutPanel1.WrapContents = false;
+			// 
 			// button1
 			// 
 			this.button1.AutoSize = true;
 			this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-			this.button1.Dock = System.Windows.Forms.DockStyle.Right;
-			this.button1.Location = new System.Drawing.Point(615, 0);
+			this.button1.Location = new System.Drawing.Point(3, 0);
+			this.button1.Margin = new System.Windows.Forms.Padding(3, 0, 0, 0);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(54, 23);
+			this.button1.Size = new System.Drawing.Size(54, 22);
 			this.button1.TabIndex = 0;
 			this.button1.Text = "Connect";
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.AutoSize = true;
+			this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.button2.Location = new System.Drawing.Point(60, 0);
+			this.button2.Margin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(41, 22);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Pause";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
 			// 
 			// timer1
 			// 
@@ -205,7 +236,7 @@
 			this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.textBox1.Location = new System.Drawing.Point(0, 0);
 			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(615, 22);
+			this.textBox1.Size = new System.Drawing.Size(568, 22);
 			this.textBox1.TabIndex = 1;
 			this.textBox1.Text = "localhost:5000";
 			this.textBox1.WordWrap = false;
@@ -234,8 +265,8 @@
 			this.treeViewAdv1.NodeControls.Add(nodeTextBox1);
 			this.treeViewAdv1.NodeControls.Add(nodeDecimalTextBox1);
 			this.treeViewAdv1.NodeControls.Add(nodeDecimalTextBox2);
-			this.treeViewAdv1.NodeControls.Add(nodeDecimalTextBox3);
-			this.treeViewAdv1.NodeControls.Add(nodeDecimalTextBox4);
+			this.treeViewAdv1.NodeControls.Add(nodeTextBox2);
+			this.treeViewAdv1.NodeControls.Add(nodeTextBox3);
 			this.treeViewAdv1.NodeControls.Add(nodeDecimalTextBox5);
 			this.treeViewAdv1.NodeControls.Add(nodeDecimalTextBox6);
 			this.treeViewAdv1.SelectedNode = null;
@@ -246,13 +277,13 @@
 			// 
 			// panel1
 			// 
-			this.panel1.Controls.Add(this.textBox1);
-			this.panel1.Controls.Add(this.button1);
-			this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-			this.panel1.Location = new System.Drawing.Point(0, 0);
-			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(669, 23);
-			this.panel1.TabIndex = 3;
+			panel1.Controls.Add(this.textBox1);
+			panel1.Controls.Add(flowLayoutPanel1);
+			panel1.Dock = System.Windows.Forms.DockStyle.Top;
+			panel1.Location = new System.Drawing.Point(0, 0);
+			panel1.Name = "panel1";
+			panel1.Size = new System.Drawing.Size(669, 23);
+			panel1.TabIndex = 3;
 			// 
 			// MemoryProfilerWindow
 			// 
@@ -260,7 +291,7 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(669, 341);
 			this.Controls.Add(this.treeViewAdv1);
-			this.Controls.Add(this.panel1);
+			this.Controls.Add(panel1);
 			this.DockAreas = ((WeifenLuo.WinFormsUI.Docking.DockAreas)((((((WeifenLuo.WinFormsUI.Docking.DockAreas.Float | WeifenLuo.WinFormsUI.Docking.DockAreas.DockLeft)
 						| WeifenLuo.WinFormsUI.Docking.DockAreas.DockRight)
 						| WeifenLuo.WinFormsUI.Docking.DockAreas.DockTop)
@@ -270,8 +301,10 @@
 			this.ShowHint = WeifenLuo.WinFormsUI.Docking.DockState.DockBottom;
 			this.TabText = "Memory profiler";
 			this.Text = "Memory profiler";
-			this.panel1.ResumeLayout(false);
-			this.panel1.PerformLayout();
+			flowLayoutPanel1.ResumeLayout(false);
+			flowLayoutPanel1.PerformLayout();
+			panel1.ResumeLayout(false);
+			panel1.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
@@ -283,7 +316,7 @@
 		private System.ComponentModel.BackgroundWorker backgroundWorker1;
 		private System.Windows.Forms.TextBox textBox1;
 		private Aga.Controls.Tree.TreeViewAdv treeViewAdv1;
-		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button2;
 	}
 }
 
