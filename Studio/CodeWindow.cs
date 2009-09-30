1fa45982f4dd45488a3c82e3c344c654cc76b665
mtlung <mtlung@080b3119-2d51-0410-af92-4d39592ae298>
2009-09-30 03:41:12 +0000

    *Use a customized version of ScintillaNet for faster control creation.

--- Studio/CodeWindow.cs
+++ Studio/CodeWindow.cs
@@ -3,6 +3,13 @@ namespace Studio
 {
 	public partial class CodeWindow : DockContent
 	{
+		/// <summary>
+		/// Using the ScintillaNet control for text editing.
+		/// Because of some problme in ScintillaNet, I've made a cutomized version,
+		/// see /mocre3d/doc folder for more information.
+		/// See: http://scintillanet.codeplex.com/WorkItem/View.aspx?WorkItemId=24671
+		/// See: http://scintillanet.codeplex.com/WorkItem/View.aspx?WorkItemId=24757
+		/// </summary>
 		public CodeWindow()
 		{
 			InitializeComponent();
