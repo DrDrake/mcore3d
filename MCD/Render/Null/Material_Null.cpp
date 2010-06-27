ecb4dd2f2abd50da2c20d8c7fbded096457a2039
mtlung <mtlung@080b3119-2d51-0410-af92-4d39592ae298>
2010-06-27 08:39:59 +0000

    *Support run-time selection of DirectX or OpenGL MCDRender.dll

---
+++ MCD/Render/Null/Material_Null.cpp
@@ -0,0 +1,28 @@
+#include "Pch.h"
+#include "../Material.h"
+
+namespace MCD {
+
+MaterialComponent::MaterialComponent()
+	: mImpl(*reinterpret_cast<Impl*>(nullptr))
+	, diffuseColor(1, 1)
+	, specularColor(1, 1)
+	, emissionColor(0, 1)
+	, specularExponent(20)
+	, opacity(1)
+{
+}
+
+MaterialComponent::~MaterialComponent()
+{
+}
+
+void MaterialComponent::preRender(size_t pass, void* context)
+{
+}
+
+void MaterialComponent::postRender(size_t pass, void* context)
+{
+}
+
+}	// namespace MCD
