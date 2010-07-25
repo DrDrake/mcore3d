86b20e707916c6f966a7931fb17ba0e5a5b8033a
mtlung <mtlung@080b3119-2d51-0410-af92-4d39592ae298>
2010-07-25 10:01:11 +0000

    *Added missing file

--- trunk/Test/RenderTest/AnimationTest.cpp
+++ trunk/Test/RenderTest/AnimationTest.cpp
@@ -1,42 +1,36 @@
 #include "Pch.h"
-#include "../RenderTest/BasicGlWindow.h"
-#include "../RenderTest/DefaultResourceManager.h"
+#include "../../MCD/Framework/Framework.h"
 #include "../../MCD/Core/Entity/Entity.h"
 #include "../../MCD/Core/Math/AnimationInstance.h"
 #include "../../MCD/Core/Math/Quaternion.h"
-#include "../../MCD/Core/System/TaskPool.h"
+#include "../../MCD/Render/Animation.h"
 #include "../../MCD/Render/ChamferBox.h"
 #include "../../MCD/Render/Mesh.h"
-#include "../../MCD/Component/Render/MeshComponent.h"
-#include "../../MCD/Component/Render/AnimationComponent.h"
+#include "../../MCD/Render/Renderer.h"
 
 using namespace MCD;
 
 namespace {
 
-class TestWindow : public BasicGlWindow
+class AnimationTestFixture
 {
 	static const size_t cFrameCount = 50;
 	static const size_t cBoxCount = 1000;
 
 public:
-	TestWindow()
-		:
-		BasicGlWindow("title=AnimationComponentTest;width=800;height=600;fullscreen=0;FSAA=4"),
-		mResourceManager(*createDefaultFileSystem())
+	AnimationTestFixture()
 	{
+		framework.initWindow("title=AnimationComponentTest;width=800;height=600;fullscreen=0;FSAA=4");
+
 		animationTrack = new AnimationTrack("");
 		loadAnimationTrack();
+
+		mBoxesNode = framework.sceneLayer().addChild(new Entity("Boxes"));
 
 		{	// Setup the chamfer box mesh
 			mesh = new Mesh("");
 			ChamferBoxBuilder chamferBoxBuilder(0.4f, 3);
 			MCD_VERIFY(mesh->create(chamferBoxBuilder, Mesh::Static));
-		}
-
-		{	// Setup the animation updater
-			updater = new AnimationUpdaterComponent(&taskPool);
-			mRootNode.addComponent(updater.get());
 		}
 
 		for(size_t i=0; i<cBoxCount; ++i)
@@ -45,32 +39,26 @@ public:
 
 	void createBox(const Vec3f& position, float initialAnimationTime)
 	{
-		// Setup entity 1
-		std::auto_ptr<Entity> e1(new Entity);
-		std::auto_ptr<Entity> e2(new Entity);
+		Entity& system = framework.systemEntity();
 
-		e1->asChildOf(&mRootNode);
-		e2->asChildOf(e1.get());
+		// Setup entity 1
+		Entity* e1 = mBoxesNode->addChild(new Entity);
+		Entity* e2 = e1->addChild(new Entity);
 
 		e1->localTransform.setTranslation(position);
 		e2->localTransform = Mat44f(Mat33f::makeXYZRotation(0, Mathf::cPiOver4(), 0));
 
 		{	// Add component
-			MeshComponent* c = new MeshComponent;
-			e2->addComponent(c);
+			MeshComponent2* c = e2->addComponent(new MeshComponent2);
 			c->mesh = mesh;
 		}
 
-		{	AnimationComponent* c = new AnimationComponent(*updater);
-			e2->addComponent(c);
+		{	AnimationUpdaterComponent* updater = system.findComponentInChildrenExactType<AnimationUpdaterComponent>();
+			MCD_ASSUME(updater);
+			AnimationComponent* c = e2->addComponent(new AnimationComponent(*updater));
 			MCD_VERIFY(c->animationInstance.addTrack(*animationTrack));
 			c->animationInstance.time = initialAnimationTime;
 		}
-
-		e1.release();
-		e2.release();
-
-		taskPool.setThreadCount(1);
 	}
 
 	void createARandomBox()
@@ -83,7 +71,7 @@ public:
 	void destroyARandomBox()
 	{
 		// Search for a random Entity to remove
-		Entity* e = mRootNode.firstChild();
+		Entity* e = mBoxesNode->firstChild();
 		size_t idx = rand() % cBoxCount;
 
 		while(idx--) {
@@ -98,25 +86,6 @@ public:
 		Quaternionf q(Mathf::random() * 2 -1, Mathf::random() * 2 -1, Mathf::random() * 2 -1, Mathf::random() * 2 -1);
 		q /= q.length();
 		return q;
-	}
-
-	sal_override void update(float deltaTime)
-	{
-		mResourceManager.processLoadingEvents();
-
-		if(mTimer.get().asSecond() > 2) {
-			loadAnimationTrack();
-			mTimer.reset();
-		}
-
-		if(Mathf::random() > 0.002)
-			createARandomBox();
-		if(Mathf::random() > 0.002)
-			destroyARandomBox();
-
-		mRootNode.localTransform.setTranslation(Vec3f(0, 0, -200));
-		RenderableComponent::traverseEntities(&mRootNode);
-		BehaviourComponent::traverseEntities(&mRootNode, deltaTime);
 	}
 
 	void loadAnimationTrack()
@@ -161,21 +130,46 @@ public:
 		reinterpret_cast<Vec3f&>(frames[cFrameCount-1]) = reinterpret_cast<Vec3f&>(frames[0]);
 	}
 
-	AnimationUpdaterComponentPtr updater;
-	Entity mRootNode;
+	Framework framework;
+	EntityPtr mBoxesNode;
 	MeshPtr mesh;
 	Timer mTimer;	// Simulate a reload of the AnimationTrack every few seconds.
 	AnimationTrackPtr animationTrack;
-	DefaultResourceManager mResourceManager;
-	TaskPool taskPool;
 };	// TestWindow
 
 }	// namespace
 
-TEST(AnimationComponentTest)
+TEST_FIXTURE(AnimationTestFixture, Render)
 {
-	TestWindow window;
-	window.mainLoop();
+	DeltaTimer deltaTimer;
+	Entity& root = framework.rootEntity();
+	RendererComponent* renderer = root.findComponentInChildrenExactType<RendererComponent>();
+	CameraComponent2* sceneCamera = root.findComponentInChildrenExactType<CameraComponent2>();
+
+	sceneCamera->entity()->localTransform.translateBy(Vec3f(0, 0, 200));
+
+	while(true)
+	{
+		Event e;
+		framework.update(e);
+
+		if(e.Type == Event::Closed)
+			break;
+
+		if(mTimer.get().asSecond() > 2) {
+			loadAnimationTrack();
+			mTimer.reset();
+		}
+
+		if(Mathf::random() > 0.002)
+			createARandomBox();
+		if(Mathf::random() > 0.002)
+			destroyARandomBox();
+
+		const float dt = (float)deltaTimer.getDelta().asSecond();
+		BehaviourComponent::traverseEntities(&root, dt);
+		renderer->render(root);
+	}
 }
 
 struct EventResult {
@@ -190,10 +184,7 @@ TEST(Event_AnimationComponentTest)
 {
 	Entity e1, e2;
 
-	TaskPool taskPool;
-	taskPool.setThreadCount(1);
-
-	AnimationUpdaterComponentPtr updater = new AnimationUpdaterComponent(&taskPool);
+	AnimationUpdaterComponentPtr updater = new AnimationUpdaterComponent(nullptr);
 	AnimationComponentPtr c = new AnimationComponent(*updater);
 
 	e1.addComponent(updater.get());
