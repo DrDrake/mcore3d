da9064d727d8aae46e40e46f127674f7b227e3a6
mtlung <mtlung@080b3119-2d51-0410-af92-4d39592ae298>
2010-10-14 17:21:28 +0000

    *Adding more binding

--- trunk/MCD/Core/Binding/CoreBindings.cpp
+++ trunk/MCD/Core/Binding/CoreBindings.cpp
@@ -85,6 +85,110 @@ SCRIPT_CLASS_REGISTER(Mat44f)
 //	.runScript("Mat44._tostring <- function(){return xBiasVector+\", \"+yBiasVector+\"), \"+zBiasVector;}")	// Vec3.tostring()
 ;}
 
+static int createVec2(HSQUIRRELVM vm)
+{
+	const int paramCount = sq_gettop(vm) - 1;
+	Vec2f* v = nullptr;
+
+	switch(paramCount) {
+	case 0:
+		v = new Vec2f(Vec2f::cZero);	// Default construct
+		break;
+	case 1:
+		if(sq_gettype(vm, 2) == OT_INSTANCE)
+			v = new Vec2f(get(TypeSelect<Vec2f&>(), vm, 2));	// Copy construct
+		else
+			v = new Vec2f(get(TypeSelect<float>(), vm, 2));	// Scalar construct
+		break;
+	case 3:
+			#define GET(i) (get(TypeSelect<float>(), vm, i))
+			v = new Vec2f(GET(2), GET(3));	// Element wise construct
+			#undef GET
+		break;
+	default:
+		return sq_throwerror(vm, "Vec2f.constructor() get incorrect number of parameters");
+	}
+
+	// Pops the input params
+	sq_pop(vm, paramCount);
+	construct::pushResult(vm, v);
+	return 1;
+}
+
+SCRIPT_CLASS_REGISTER(Vec2f)
+	.declareClass<Vec2f>("Vec2")
+	.rawMethod("constructor", createVec2)
+	.var("x", (float Vec2f::*)&Vec2f::x)
+	.var("y", (float Vec2f::*)&Vec2f::y)
+	.runScript("Vec2._tostring <- function(){return x+\", \"+y+\";}")
+;}
+
+static int createVec3(HSQUIRRELVM vm)
+{
+	const int paramCount = sq_gettop(vm) - 1;
+	Vec3f* v = nullptr;
+
+	switch(paramCount) {
+	case 0:
+		v = new Vec3f(Vec3f::cZero);	// Default construct
+		break;
+	case 1:
+		if(sq_gettype(vm, 2) == OT_INSTANCE)
+			v = new Vec3f(get(TypeSelect<Vec3f&>(), vm, 2));	// Copy construct
+		else
+			v = new Vec3f(get(TypeSelect<float>(), vm, 2));	// Scalar construct
+		break;
+	case 3:
+			#define GET(i) (get(TypeSelect<float>(), vm, i))
+			v = new Vec3f(GET(2), GET(3), GET(4));	// Element wise construct
+			#undef GET
+		break;
+	default:
+		return sq_throwerror(vm, "Vec3f.constructor() get incorrect number of parameters");
+	}
+
+	// Pops the input params
+	sq_pop(vm, paramCount);
+	construct::pushResult(vm, v);
+	return 1;
+}
+
+static float lengthVec3(const Vec3f& v) { return v.length(); }
+static Vec3f addVec3(const Vec3f& lhs, const Vec3f& rhs) { return lhs + rhs; }
+static Vec3f subVec3(const Vec3f& lhs, const Vec3f& rhs) { return lhs - rhs; }
+static Vec3f mulVec3(const Vec3f& lhs, float rhs) { return lhs * rhs; }
+static Vec3f unmVec3(const Vec3f& lhs) { return -lhs; }
+static int cmpVec3(const Vec3f& lhs, const Vec3f& rhs) { if(lhs == rhs) return 0; return 1;/*return lhs > rhs ? 1 : -1;*/ }
+static bool isEqualVec3(const Vec3f& lhs, const Vec3f& rhs) { return lhs == rhs; }
+static void addEqualVec3(Vec3f& lhs, const Vec3f& rhs) { lhs += rhs; }
+static void mulEqualVec3(Vec3f& lhs, float rhs) { lhs *= rhs; }
+
+SCRIPT_CLASS_REGISTER(Vec3f)
+	.declareClass<Vec3f>("Vec3")
+	.rawMethod("constructor", createVec3)
+	.var("x", (float Vec3f::*)&Vec3f::x)
+	.var("y", (float Vec3f::*)&Vec3f::y)
+	.var("z", (float Vec3f::*)&Vec3f::z)
+	.method("dot", &Vec3f::dot)
+	.method("_modulo", &Vec3f::dot)
+	.varGet("norm", &Vec3f::norm)
+	.method("cross", &Vec3f::operator^)
+	.varGet("length", &lengthVec3)	// Vec3f::length() didn't return float
+	.varGet("squaredLength", &Vec3f::squaredLength)
+	.method("distance", &Vec3f::distance)
+	.method("squaredDistance", &Vec3f::squaredDistance)
+	.method("normalize", &Vec3f::normalizeSafe)
+	.method("_add", &addVec3)
+	.method("_sub", &subVec3)
+	.method("_mul", &mulVec3)
+	.method("_unm", &unmVec3)
+	.method("_cmp", &cmpVec3)
+	.method("isEqual", &isEqualVec3)
+	.method("addEqual", &addEqualVec3)
+	.method("mulEqual", &mulEqualVec3)
+	.runScript("Vec3._tostring <- function(){return x+\", \"+y+\", \"+z;}")
+;}
+
 // Entity
 
 SCRIPT_CLASS_REGISTER(Entity)
