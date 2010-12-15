5faaffbda7e78a8174c27f63547ce2dc5cb35ee7
mtlung <mtlung@080b3119-2d51-0410-af92-4d39592ae298>
2010-12-15 06:55:27 +0000

    *Added formatStr() to StrUtility.h

--- trunk/MCD/Core/System/StrUtility.h
+++ trunk/MCD/Core/System/StrUtility.h
@@ -7,11 +7,14 @@
 
 namespace MCD {
 
-//! Convert narrow string into wide string using the current locale, return true on success.
+/// Create a std::string with printf like formatting, may not be high performance but handy.
+MCD_CORE_API std::string formatStr(sal_format_guard const char* fmt, ...);
+
+/// Convert narrow string into wide string using the current locale, return true on success.
 MCD_CORE_API sal_checkreturn bool strToWStr(sal_in_z sal_in_ecount(maxCount) const char* narrowStr, size_t maxCount, std::wstring& wideStr);
 MCD_CORE_API sal_checkreturn bool strToWStr(const std::string& narrowStr, std::wstring& wideStr);
 
-//! Convert wide string into narrow string using the current locale, return true on success.
+/// Convert wide string into narrow string using the current locale, return true on success.
 MCD_CORE_API sal_checkreturn bool wStrToStr(sal_in_z sal_in_ecount(maxCount) const wchar_t* wideStr, size_t maxCount, std::string& narrowStr);
 MCD_CORE_API sal_checkreturn bool wStrToStr(const std::wstring& wideStr, std::string& narrowStr);
 
@@ -19,28 +22,28 @@ MCD_CORE_API std::wstring strToWStr(const std::string& narrowStr);
 
 MCD_CORE_API std::string wStrToStr(const std::wstring& wideStr);
 
-//! Convert utf-8 string into wide string, return true on success.
+/// Convert utf-8 string into wide string, return true on success.
 MCD_CORE_API sal_checkreturn bool utf8ToWStr(sal_in_z sal_in_ecount(maxCount) const char* utf8Str, size_t maxCount, std::wstring& wideStr);
 MCD_CORE_API sal_checkreturn bool utf8ToWStr(const std::string& utf8Str, std::wstring& wideStr);
 
-//! Convert wide string into utf-8 string, return true on success.
+/// Convert wide string into utf-8 string, return true on success.
 MCD_CORE_API sal_checkreturn bool wStrToUtf8(sal_in_z sal_in_ecount(maxCount) const wchar_t* wideStr, size_t maxCount, std::string& utf8Str);
 MCD_CORE_API sal_checkreturn bool wStrToUtf8(const std::wstring& wideStr, std::string& utf8Str);
 
-//! Number to string.
+/// Number to string.
 MCD_CORE_API std::string int2Str(int number);
 MCD_CORE_API std::string float2Str(float number);
 MCD_CORE_API std::string double2Str(double number);
 
-//! Number array to string.
+/// Number array to string.
 MCD_CORE_API std::string intArray2Str(sal_in sal_in_ecount(count) const int* numbers, size_t count);
 MCD_CORE_API std::string floatArray2Str(sal_in sal_in_ecount(count) const float* numbers, size_t count);
 
-//! String to number.
+/// String to number.
 MCD_CORE_API sal_checkreturn bool str2Int(sal_in_z const char* str, int& number);
 MCD_CORE_API sal_checkreturn bool str2Double(sal_in_z const char* str, double& number);
 
-//! String to number with a supplied default value in case the parse failed.
+/// String to number with a supplied default value in case the parse failed.
 MCD_CORE_API int str2IntWithDefault(sal_in_z const char* str, int defaultVal);
 MCD_CORE_API int str2DoubleWithDefault(sal_in_z const char* str, double defaultVal);
 
@@ -53,10 +56,10 @@ MCD_CORE_API int str2DoubleWithDefault(sal_in_z const char* str, double defaultV
 MCD_CORE_API sal_maybenull int* strToIntArray(sal_in_z const char* str, sal_inout size_t& size);
 MCD_CORE_API sal_maybenull float* strToFloatArray(sal_in_z const char* str, sal_inout size_t& size);
 
-//! Compare two character strings, ignoring case.
+/// Compare two character strings, ignoring case.
 MCD_CORE_API sal_checkreturn int strCaseCmp(sal_in_z const char* string1, sal_in_z const char* string2);
 
-//! Compare two wide-character strings, ignoring case.
+/// Compare two wide-character strings, ignoring case.
 MCD_CORE_API sal_checkreturn int wstrCaseCmp(sal_in_z const wchar_t* string1, sal_in_z const wchar_t* string2);
 
 /*!	A simple name value pair parser.
